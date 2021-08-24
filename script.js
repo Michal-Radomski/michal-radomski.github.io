4376f84ab0260f5b0454c0a75832580df1edf78b
Michal Radomski <michalra@op.pl>
2021-08-24 22:16:01 +0200

    topNav was done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+function ResponsiveMenu() {
+  let nav_top = document.getElementById("topNav");
+  if (nav_top.className.indexOf("w3-show") == -1) {
+    nav_top.className += " w3-show";
   } else {
-    x.className = x.className.replace(" w3-show", "");
+    nav_top.className = nav_top.className.replace(" w3-show", "");
   }
 }
