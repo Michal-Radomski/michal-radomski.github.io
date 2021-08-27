e5f32596f83d289acb714cc3f7d0d8347707c19e
Michal Radomski <michalra@op.pl>
2021-08-27 19:46:07 +0200

    randomColor.js was added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Used to toggle the menu on small screens when clicking on the menu button
+// Used to toggle the menu on small screens when clicking on the menu button - W3 Template
 function ResponsiveMenu() {
   let nav_top = document.getElementById("topNav");
   if (nav_top.className.indexOf("w3-show") == -1) {
@@ -7,3 +7,9 @@ function ResponsiveMenu() {
     nav_top.className = nav_top.className.replace(" w3-show", "");
   }
 }
+
+//Adding random BGC for header > h2
+let randomColors = randomColor({count: 3, luminosity: "light", alpha: 0.8});
+// console.log("randomColors:", randomColors);
+document.querySelector("h1.title").style.textShadow = `0px 0px 35px ${randomColors[0]}, 0px 0px 35px ${randomColors[0]},
+0px 0px 35px ${randomColors[0]}, 0px 0px 35px ${randomColors[0]}`;
