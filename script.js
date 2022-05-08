ca821e6845d0a9827d9b04ebbae12a66182e8e46
Michal Radomski <michalra@op.pl>
2022-05-08 13:35:51 +0200

    Responsiveness fixed2

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Used to toggle the menu on small screens when clicking on the menu button - W3 Template
 function ResponsiveMenu() {
   let nav_top = document.getElementById("topNav");
+  // console.log("nav_top:", nav_top);
+  // console.log("nav_top.className:", nav_top.className);
   if (nav_top.className.indexOf("w3-show") == -1) {
     nav_top.className += " w3-show";
   } else {
     nav_top.className = nav_top.className.replace(" w3-show", "");
   }
+  // console.log("nav_top.className.indexOf('w3-show'):", nav_top.className.indexOf("w3-show"));
 }
 
 // Number of apps
